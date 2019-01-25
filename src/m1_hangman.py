b4744ca32c1b0366d721f761d11f75dc7d862ef3
wus4 <wus4@rose-hulman.edu>
2019-01-25 00:15:57 -0500

    .

--- src/m1_hangman.py
+++ src/m1_hangman.py
@@ -1,50 +1,80 @@
 """
 Hangman.
 
-Authors: PUT_YOUR_NAME_HERE and YOUR_PARTNERS_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+Authors: Shixin Wu.
+"""  # done: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
-# TODO: 2. Implement Hangman using your Iterative Enhancement Plan.
+# done: 2. Implement Hangman using your Iterative Enhancement Plan.
 
 ####### Do NOT attempt this assignment before class! #######
-def main():
-    w=initate()
-    game(w)
-    play_again()
 
+import random
+
+def main():
+    word=initate()
+    print('_ ' * len(word))
+    att = int(input('Please enter your attempts: '))
+    res = game(word,att)
+    result(res, word)
+    while play_again()=='y':
+        w = initate()
+        res=game(word,att)
+        result(res,word)
 
 
 
 def initate():
-    word='apple'
+    with open('words.txt') as f:
+        f.readline()
+        string=f.read()
+        words=string.split()
+    r=random.randrange(0,len(words))
+    word=words[r]
+    # word='apple'
     return word
 
 
-
-def game(word):
+def game(word,att):
     lose=0
     win=0
-    blank=list('_ '*len(word))
-    print('_ '*len(word))
-    while lose<5 or win<5:
+    blank=list('_'*len(word))
+    loset=0
+    lastletter=True
+    while lose<att and win<len(word):
         letter=input('Please guess a letter: ')
+
+        while lastletter==letter:
+            letter = input('Please guess another letter: ')
+        lastletter = letter
         for k in range(len(word)):
             if word[k]==letter:
                 win=win+1
                 blank[k]=word[k]
             else:
-                lose=lose+1
-        # print('you have ',lose,'times of failure.')
-        print(''.join(blank), ' ')
-    if lose==-5:
-        print('lose')
-    if win==5:
+                loset=loset+1
+        print(' '.join(blank))
+
+        if loset==len(word):
+            lose=lose+1
+            print('wrong! attempts left: ',att-lose)
+        else:
+            print('correct! attempts left:', att - lose)
+        loset=0
+    return (win,lose)
+
+def result(res,word):
+    if res[0]==len(word):
         print('win')
+    else:
+        print('lose. The correct word is: '+word)
+
+
+
 
 def play_again():
     if input('play again: y/n?')=='y':
-        game(w)
+        return 'y'
     else:
-        print('out')
+        print('Thank you for playing hangman!')
 main()
 
