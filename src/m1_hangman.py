cae0530a3c0c12d7f3b8780eaa3c60797336a4f7
wus4 <wus4@rose-hulman.edu>
2019-01-24 19:02:09 -0500

    .

--- src/m1_hangman.py
+++ src/m1_hangman.py
@@ -7,4 +7,44 @@ Authors: PUT_YOUR_NAME_HERE and YOUR_PARTNERS_NAME_HERE.
 # TODO: 2. Implement Hangman using your Iterative Enhancement Plan.
 
 ####### Do NOT attempt this assignment before class! #######
+def main():
+    w=initate()
+    game(w)
+    play_again()
+
+
+
+
+def initate():
+    word='apple'
+    return word
+
+
+
+def game(word):
+    lose=0
+    win=0
+    blank=list('_ '*len(word))
+    print('_ '*len(word))
+    while lose<5 or win<5:
+        letter=input('Please guess a letter: ')
+        for k in range(len(word)):
+            if word[k]==letter:
+                win=win+1
+                blank[k]=word[k]
+            else:
+                lose=lose+1
+        # print('you have ',lose,'times of failure.')
+        print(''.join(blank), ' ')
+    if lose==-5:
+        print('lose')
+    if win==5:
+        print('win')
+
+def play_again():
+    if input('play again: y/n?')=='y':
+        game(w)
+    else:
+        print('out')
+main()
 
